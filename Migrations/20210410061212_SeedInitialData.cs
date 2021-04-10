af85560c3eccc5c56f9db1ee66811b7c9e8b5ff2
Gerardo Ramírez <gerardo.rmz98@hotmail.com>
2021-04-10 01:14:36 -0500

    Seed DB Initial Data

---
+++ Migrations/20210410061212_SeedInitialData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleCrudAPI.Migrations
+{
+    public partial class SeedInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Suppliers",
+                columns: new[] { "ID", "Name", "Phone" },
+                values: new object[] { 1, "Fake Supplier #1", "123456789" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "ID", "Email", "IsAdmin" },
+                values: new object[] { 1, "admin@test.com", true });
+
+            migrationBuilder.InsertData(
+                table: "Receipts",
+                columns: new[] { "ID", "Amount", "Comments", "Date", "SupplierID", "UserID" },
+                values: new object[] { 1, 1000m, "First Receipt!", new DateTime(2021, 4, 10, 1, 12, 12, 271, DateTimeKind.Local).AddTicks(9878), 1, 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Receipts",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Suppliers",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
--- Models/SimpleCrudDBContext.cs
+++ Models/SimpleCrudDBContext.cs
@@ -17,5 +17,12 @@ namespace SimpleCrudAPI.Models
         {
             Database.Migrate();
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User { ID = 1, Email = "admin@test.com", IsAdmin = true });
+            modelBuilder.Entity<Supplier>().HasData(new Supplier { ID = 1, Name = "Fake Supplier #1", Phone = "123456789" });
+            modelBuilder.Entity<Receipt>().HasData(new Receipt { ID = 1, UserID= 1, SupplierID = 1, Date = DateTime.Now, Amount = 1000, Comments = "First Receipt!" });
+        }
     }
 }
